3803281eabbc4aea0deeffc820bed0462301b3c9
Ashly <ashlyalva@gmail.com>
2023-10-03 20:37:44 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TareaOperacionesBasicas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace TareaOperacionesBasicas
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Presiona {count} vez";
+            else
+                CounterBtn.Text = $"Presiona {count} veces";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
